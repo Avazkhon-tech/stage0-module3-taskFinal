eb296edd851ec0487d3c5d4f21927759f9ebea92
Avazkhon <avazxonnazirov334@gmail.com>
2024-08-13 15:37:21 +0500

    first commit

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int newNum = 0;
+        while (number != 0) {
+            int lastDigit = number % 10;
+            newNum = newNum * 10 + lastDigit;
+            number /= 10;
+        }
+        System.out.println(newNum);
     }
 }
